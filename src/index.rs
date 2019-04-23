7f06c0170bbe71c0bbc5341c02bc9c1b3fef480a
Chris Konstad <chriskon149@gmail.com>
2019-04-22 20:42:46 -0700

    Ran rustfmt

--- src/index.rs
+++ src/index.rs
@@ -1,21 +1,25 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #![feature(vec_remove_item)]
 
-#[macro_use] extern crate log;
-#[macro_use] extern crate rocket;
-#[macro_use] extern crate rocket_contrib;
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
 
 extern crate strsim;
 
-use rocket_contrib::json::{Json, JsonValue};
 use rocket::State;
+use rocket_contrib::json::{Json, JsonValue};
 use strsim::normalized_damerau_levenshtein;
 
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
-use std::sync::{Arc, Mutex};
-use std::collections::HashMap;
 
 // TODO share these common structs between index + db
 
@@ -26,8 +30,8 @@ struct Match {
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 enum Event {
-    Set{key: String, value: String},
-    Delete{key: String},
+    Set { key: String, value: String },
+    Delete { key: String },
 }
 
 type MessageMap = HashMap<String, Vec<String>>;
@@ -38,10 +42,7 @@ struct AppState {
 }
 
 #[get("/<key>/<similarity>", format = "json")]
-fn get(
-    key: String,
-    similarity: f64,
-    state : State<Arc<Mutex<AppState>>>) -> Option<Json<Match>> {
+fn get(key: String, similarity: f64, state: State<Arc<Mutex<AppState>>>) -> Option<Json<Match>> {
     let hashmap = &state.lock().unwrap().db;
     let mut results = Vec::<String>::new();
     for k in hashmap.keys() {
@@ -49,12 +50,21 @@ fn get(
             results.push(k.clone());
         }
     }
-    results.sort_by(|a, b|
-        return normalized_damerau_levenshtein(&key, a).partial_cmp(
-            &normalized_damerau_levenshtein(&key, b)).unwrap());
+    results.sort_by(|a, b| {
+        return normalized_damerau_levenshtein(&key, a)
+            .partial_cmp(&normalized_damerau_levenshtein(&key, b))
+            .unwrap();
+    });
     match results.len() {
         0 => None,
-        _ => Some(Json(Match{keys: results.into_iter().flat_map(|k| hashmap.get(&k).into_iter()).flat_map(|x| x.into_iter()).cloned().collect()})),
+        _ => Some(Json(Match {
+            keys: results
+                .into_iter()
+                .flat_map(|k| hashmap.get(&k).into_iter())
+                .flat_map(|x| x.into_iter())
+                .cloned()
+                .collect(),
+        })),
     }
 }
 
@@ -68,7 +78,7 @@ fn not_found() -> JsonValue {
 
 fn apply(event: Event, state: &mut MessageMap) {
     match event {
-        Event::Set{key, value} => {
+        Event::Set { key, value } => {
             // Delete old entry
             let mut keys_to_delete = vec![];
             for (k, v) in state.iter_mut() {
@@ -86,7 +96,7 @@ fn apply(event: Event, state: &mut MessageMap) {
             let entry = state.entry(value).or_insert(Vec::new());
             entry.push(key);
         }
-        Event::Delete{key} => {
+        Event::Delete { key } => {
             let mut keys_to_delete = vec![];
             for (k, v) in state.iter_mut() {
                 v.remove_item(&key);
@@ -103,35 +113,34 @@ fn apply(event: Event, state: &mut MessageMap) {
 }
 
 fn background_update(state: Arc<Mutex<AppState>>) {
-    thread::spawn(move || {
-        loop {
-            {
-                let mut s = state.lock().unwrap();
-                let url = format!("http://localhost:8000/log/{}", s.offset);
-                let events = reqwest::Client::new()
-                    .get(&*url)
-                    .send()
-                    .unwrap()
-                    .json::<Vec<Event>>().unwrap();
-                let num_events = events.len();
-                if num_events != 0 {
-                    info!("Got {} new events", num_events);
-                }
-                for e in events.into_iter() {
-                    info!("{:#?}", e);
-                    apply(e, &mut s.db);
-                }
-                s.offset += num_events;
+    thread::spawn(move || loop {
+        {
+            let mut s = state.lock().unwrap();
+            let url = format!("http://localhost:8000/log/{}", s.offset);
+            let events = reqwest::Client::new()
+                .get(&*url)
+                .send()
+                .unwrap()
+                .json::<Vec<Event>>()
+                .unwrap();
+            let num_events = events.len();
+            if num_events != 0 {
+                info!("Got {} new events", num_events);
             }
-            thread::sleep(Duration::from_millis(1000));
+            for e in events.into_iter() {
+                info!("{:#?}", e);
+                apply(e, &mut s.db);
+            }
+            s.offset += num_events;
         }
+        thread::sleep(Duration::from_millis(1000));
     });
 }
 
 fn main() {
-    let state = Arc::new(Mutex::new(AppState{
-            db: HashMap::<String, Vec<String>>::new(),
-            offset: 0,
+    let state = Arc::new(Mutex::new(AppState {
+        db: HashMap::<String, Vec<String>>::new(),
+        offset: 0,
     }));
     background_update(Arc::clone(&state));
     rocket::ignite()
